169e3117f698bfba627d25198efc2c08c5a4c67a
Phonethip <phonethip.liu@gmail.com>
2018-03-20 01:55:10 -0500

    working on dom-builder.js to output to the game to the dom

---
+++ js/dom-builder.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let $ = require('jquery');
+
+// ***** how do I fix this? ******** //
+
+// function makeGameQuestions(game){
+//     let gameDisplay = 
+//     $(`<div class="col-sm-8 quiz-display" id="quiz-display-area">
+//         <label> ${game.type}</label>
+//         <h2>${game.name}</h2>
+//         <ol class= "game-questions">
+//         </ol>
+//         </div>`);
+
+//     $(".quiz-display").html(makeGameQuestions);
+//     for(let question in game){
+//         let currentGame = game[question],
+//             gametype = $("<li>", {class: "game_title"}),
+//             question = $("<h5>", {class: "game_question"}).text(currentGame.title), 
+//             answerOptions = $("</h5>"),
+//     }
+// };
+
+
+//             `<li> 
+//                 <h5>Will you be the one?</h5>
+                
+//                     <div class="form-check">
+//                         <input class="btn btn-outline-secondary btn-sm" type="button" value="Answer 1" checked>
+//                         <input class="btn btn-outline-secondary btn-sm" type="button" value="Answer 2">
+//                         <input class="btn btn-outline-secondary btn-sm" type="button" value="Answer 3">
+//                     </div>
+//                 </li>`
+
+
+//     `);
+// }
